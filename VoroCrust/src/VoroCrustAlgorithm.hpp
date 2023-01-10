8dcb7ff2a912fc600079636617215b7b7409baec
ItamarShmelo <itamargiron@gmail.com>
2023-01-10 16:56:57 +0200

    Declaration of VoroCrust Algorithm class

---
+++ VoroCrust/src/VoroCrustAlgorithm.hpp
@@ -0,0 +1,15 @@
+#ifndef VOROCRUST_ALGORITHM
+#define VOROCRUST_ALGORITHM
+
+#include "PL_Complex.hpp"
+
+class VoroCrustAlgorithm {
+    public:
+        PL_Complex plc;
+
+        VoroCrustAlgorithm(PL_Complex const& plc_);
+
+}
+
+
+#endif /* VOROCRUST_ALGORITHM */
