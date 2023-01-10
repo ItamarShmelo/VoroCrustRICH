52bd565f84a1ee4bb346d2419a46d1462b524158
ItamarShmelo <itamargiron@gmail.com>
2023-01-10 16:23:06 +0200

    Add Face creates a face using indices of existing vertices adds it to it's own vector of faces and to invokes addFace of the vertices of the new face.

--- VoroCrust/src/PL_Complex.cpp
+++ VoroCrust/src/PL_Complex.cpp
@@ -6,6 +6,21 @@ PL_Complex::PL_Complex(std::vector<Vector3D> const& vertices_) : vertices(), fac
         vertices.push_back(std::make_shared<VoroCrustVertex>(vertex));
 }
 
+void PL_Complex::addFace(std::vector<unsigned int> const& indices){
+
+    std::vector<std::shared_ptr<VoroCrustVertex>> face_vertices = std::vector<std::shared_ptr<VoroCrustVertex>>();
+
+    for(auto& index : indices)
+        face_vertices.push_back(vertices[index]);
+
+    std::shared_ptr<VoroCrustFace> new_face_ptr = std::make_shared<VoroCrustFace>(VoroCrustFace(face_vertices));
+
+    for (std::shared_ptr<VoroCrustVertex> vertex_ptr : face_vertices){
+        vertex_ptr->addFace(new_face_ptr);
+    }
+
+    faces.push_back(new_face_ptr);
+}
 
 std::string PL_Complex::repr() const{
     std::ostringstream s;
