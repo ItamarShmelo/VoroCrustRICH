cccbbc2ee4d6dbe9d6dc16b14150661756538ec0
ItamarShmelo <itamargiron@gmail.com>
2023-01-11 15:53:17 +0200

    initializatrion now has an index

--- VoroCrust/src/PL_Complex.cpp
+++ VoroCrust/src/PL_Complex.cpp
@@ -1,9 +1,11 @@
 #include "PL_Complex.hpp"
 
 PL_Complex::PL_Complex(std::vector<Vector3D> const& vertices_) : vertices(), faces(){
-    
-    for(auto& vertex : vertices_) 
-        vertices.push_back(std::make_shared<VoroCrustVertex>(vertex));
+    std::size_t index = 0;
+    for(auto& vertex : vertices_) {
+        vertices.push_back(std::make_shared<VoroCrustVertex>(vertex, index));
+        index++;
+    }
 }
 
 void PL_Complex::addFace(std::vector<unsigned int> const& indices){
