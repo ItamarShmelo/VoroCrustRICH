0fca8392442147acbd807aaa4329c6ffda4382cb
ItamarShmelo <itamargiron@gmail.com>
2023-01-11 19:13:59 +0200

    decleration of variables of edge and functions

--- VoroCrust/src/VoroCrustEdge.hpp
+++ VoroCrust/src/VoroCrustEdge.hpp
@@ -13,6 +13,21 @@ class VoroCrustFace;
 class VoroCrustEdge
 {
 
+public:
+    std::shared_ptr<VoroCrustVertex> vertex1, vertex2;
+    std::vector<std::shared_ptr<VoroCrustFace>> faces;
+    
+    std::size_t index;
+
+    VoroCrustEdge(std::shared_ptr<VoroCrustVertex> const& v1, std::shared_ptr<VoroCrustVertex> const& v2, std::size_t const index_);
+    
+    ~VoroCrustEdge() = default;
+
+    bool checkIfEqual(std::shared_ptr<VoroCrustVertex> const& v1, std::shared_ptr<VoroCrustVertex> const& v2);
+
+    void addFace(std::shared_ptr<VoroCrustFace> new_face);
+
+    std::string repr();
 };
 
 #endif /* VOROCRUST_EDGE */ 
