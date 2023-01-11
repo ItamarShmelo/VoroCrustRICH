7ddaec97001f9449178408eb40c7a99ae0ac8a5b
ItamarShmelo <itamargiron@gmail.com>
2023-01-11 19:12:58 +0200

    Declaration of VoroCrustEdge

---
+++ VoroCrust/src/VoroCrustEdge.hpp
@@ -0,0 +1,18 @@
+#ifndef VOROCRUST_EDGE
+#define VOROCRUST_EDGE
+
+#include <vector>
+#include <memory>
+#include <string>
+#include "VoroCrustVertex.hpp"
+#include "VoroCrustFace.hpp"
+
+class VoroCrustVertex;
+class VoroCrustFace;
+
+class VoroCrustEdge
+{
+
+};
+
+#endif /* VOROCRUST_EDGE */ 
