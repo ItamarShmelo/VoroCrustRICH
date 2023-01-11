c6fed3dab4a74f61a980c31ac6ef9496a7ead864
ItamarShmelo <itamargiron@gmail.com>
2023-01-11 19:20:10 +0200

    added edge vector to VoroCrustVertex plus addEdge function

--- VoroCrust/src/VoroCrustVertex.hpp
+++ VoroCrust/src/VoroCrustVertex.hpp
@@ -6,20 +6,26 @@
 #include <memory>
 #include "../../source/3D/GeometryCommon/Vector3D.hpp"
 #include "VoroCrustFace.hpp"
+#include "VoroCrustEdge.hpp"
 class VoroCrustFace;
+class VoroCrustEdge;
 
 class VoroCrustVertex
 {
     public:
         Vector3D vertex;
         std::vector<std::shared_ptr<VoroCrustFace>> faces;
+        std::vector<std::shared_ptr<VoroCrustEdge>> edges;
         std::size_t index;
 
         VoroCrustVertex(Vector3D const& vertex_, std::size_t const index_);
 
         ~VoroCrustVertex() = default;
-        void addFace(std::shared_ptr<VoroCrustFace> new_face);
         
+        void addFace(std::shared_ptr<VoroCrustFace> new_face);
+
+        void addEdge(std::shared_ptr<VoroCrustEdge> const& new_edge) {edges.push_back(new_edge);}
+
         std::string repr() const;
 };
 #endif /* VOROCRUST_VERTEX_HPP */
