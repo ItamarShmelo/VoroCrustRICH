c41363cd38dbb1cf3cc480aaca99f0ffcac9c627
ItamarShmelo <itamargiron@gmail.com>
2023-01-17 15:31:04 +0200

    formatting

--- VoroCrust/src/PL_Complex.cpp
+++ VoroCrust/src/PL_Complex.cpp
@@ -11,21 +11,25 @@ PL_Complex::PL_Complex(std::vector<Vector3D> const& vertices_) : vertices(),
                                                                  sharp_corners(),
                                                                  creases() {
     std::size_t index = 0;
-    for(auto& vertex : vertices_) {
+    for (auto &vertex : vertices_)
+    {
         vertices.push_back(std::make_shared<VoroCrustVertex>(vertex, index));
         index++;
     }
 }
 
-Edge PL_Complex::addEdge(Vertex const& v1, Vertex const& v2){
-    for (auto& edge : edges){
-        if (edge->checkIfEqual(v1, v2)){
+Edge PL_Complex::addEdge(Vertex const &v1, Vertex const &v2)
+{
+    for (auto &edge : edges)
+    {
+        if (edge->checkIfEqual(v1, v2))
+        {
             return edge;
         }
     }
 
     auto new_edge_ptr = std::make_shared<VoroCrustEdge>(v1, v2, edges.size());
-    
+
     new_edge_ptr->vertex1->addEdge(new_edge_ptr);
     new_edge_ptr->vertex2->addEdge(new_edge_ptr);
 
@@ -33,35 +37,41 @@ Edge PL_Complex::addEdge(Vertex const& v1, Vertex const& v2){
     return new_edge_ptr;
 }
 
-void PL_Complex::addFace(std::vector<unsigned int> const& indices){
+void PL_Complex::addFace(std::vector<unsigned int> const &indices)
+{
 
-    if (indices.size() != unique(indices).size()){
+    if (indices.size() != unique(indices).size())
+    {
         std::cout << "ERROR: Repeated indices in PL_Complex::addFace!" << std::endl;
         exit(1);
     }
 
-    if(indices.size() < 3){
+    if (indices.size() < 3)
+    {
         std::cout << "ERROR: Face has to have at least three vertices!" << std::endl;
         exit(1);
     }
 
     std::vector<Vertex> face_vertices = std::vector<Vertex>();
 
-    for(auto& index : indices)
+    for (auto &index : indices)
         face_vertices.push_back(vertices[index]);
 
     Face new_face_ptr = std::make_shared<VoroCrustFace>(face_vertices, faces.size());
 
-    for (Vertex vertex_ptr : face_vertices){
+    for (Vertex vertex_ptr : face_vertices)
+    {
         vertex_ptr->addFace(new_face_ptr);
     }
 
-    for (std::size_t i = 0; i < new_face_ptr->vertices.size(); ++i){
-        auto new_edge_ptr = this->addEdge(new_face_ptr->vertices[i], new_face_ptr->vertices[ (i+1) % (new_face_ptr->vertices.size())]);
+    for (std::size_t i = 0; i < new_face_ptr->vertices.size(); ++i)
+    {
+        auto new_edge_ptr = this->addEdge(new_face_ptr->vertices[i], new_face_ptr->vertices[(i + 1) % (new_face_ptr->vertices.size())]);
         new_edge_ptr->addFace(new_face_ptr);
         new_face_ptr->addEdge(new_edge_ptr);
-        
-        if(new_edge_ptr->faces.size() == 2){
+
+        if (new_edge_ptr->faces.size() == 2)
+        {
             new_face_ptr->neighbors.push_back(new_edge_ptr->faces[0]);
             new_edge_ptr->faces[0]->neighbors.push_back(new_edge_ptr->faces[1]);
 
@@ -72,9 +82,11 @@ void PL_Complex::addFace(std::vector<unsigned int> const& indices){
     faces.push_back(new_face_ptr);
 }
 
-bool PL_Complex::checkAllVerticesAreOnFace(){
-    for (auto& vertex : vertices)
-        if (vertex->faces.size() == 0){
+bool PL_Complex::checkAllVerticesAreOnFace()
+{
+    for (auto &vertex : vertices)
+        if (vertex->faces.size() == 0)
+        {
             std::cout << "ERROR: vertex " << vertex->index << " is not part of a face!!";
             return false;
         }
@@ -82,31 +94,36 @@ bool PL_Complex::checkAllVerticesAreOnFace(){
     return true;
 }
 
-bool PL_Complex::checkIfALLFacesAreFlat(){
-    for (auto& face : faces){
+bool PL_Complex::checkIfALLFacesAreFlat()
+{
+    for (auto &face : faces)
+    {
         // if face is a triangle hence flat by definition cycle
-        if(face->vertices.size() == 3) continue;
+        if (face->vertices.size() == 3)
+            continue;
 
         // span{v1, v2} define the plane the face is assumed to be on. v2 is orthogonal to v1.
-        Vector3D const& v1 = face->vertices[1]->vertex - face->vertices[0]->vertex;
-        Vector3D const& v_temp = face->vertices[2]->vertex - face->vertices[1]->vertex;
-        Vector3D const& v2 = v_temp - (ScalarProd(v1, v_temp)/ScalarProd(v1, v1))*v1;
+        Vector3D const &v1 = face->vertices[1]->vertex - face->vertices[0]->vertex;
+        Vector3D const &v_temp = face->vertices[2]->vertex - face->vertices[1]->vertex;
+        Vector3D const &v2 = v_temp - (ScalarProd(v1, v_temp) / ScalarProd(v1, v1)) * v1;
 
         std::cout << "ScalarProduct(v1, v2) = " << ScalarProd(v2, v1) << std::endl;
-        
-        for(std::size_t i=2; i<face->vertices.size(); ++i){
-            Vector3D const& v = face->vertices[(i+1)%face->vertices.size()]->vertex - face->vertices[i]->vertex;
-            Vector3D const& v_perp = v - ((ScalarProd(v,v1) / ScalarProd(v1,v1)) * v1 + (ScalarProd(v,v2) / ScalarProd(v2,v2)) * v2);
 
-            std::cout << "Edge: " << i+1 << " size of perp : " << ScalarProd(v_perp, v_perp) << std::endl;
+        for (std::size_t i = 2; i < face->vertices.size(); ++i)
+        {
+            Vector3D const &v = face->vertices[(i + 1) % face->vertices.size()]->vertex - face->vertices[i]->vertex;
+            Vector3D const &v_perp = v - ((ScalarProd(v, v1) / ScalarProd(v1, v1)) * v1 + (ScalarProd(v, v2) / ScalarProd(v2, v2)) * v2);
 
-            if(ScalarProd(v_perp, v_perp) > 1e-14){
+            std::cout << "Edge: " << i + 1 << " size of perp : " << ScalarProd(v_perp, v_perp) << std::endl;
+
+            if (ScalarProd(v_perp, v_perp) > 1e-14)
+            {
                 std::cout << "ERROR: face is not flat! Make sure all the vertices of face " << face->index << " are on the same plane!!" << std::endl;
-                std::cout << "Edge: " << i+1 << " size of perp : " << ScalarProd(v_perp, v_perp) << std::endl;
+                std::cout << "Edge: " << i + 1 << " size of perp : " << ScalarProd(v_perp, v_perp) << std::endl;
                 std::cout << "v = " << v.x << ", " << v.y << ", " << v.z << ", " << std::endl;
                 std::cout << "v1 = " << v1.x << ", " << v1.y << ", " << v1.z << ", " << std::endl;
                 std::cout << "v2 = " << v2.x << ", " << v2.y << ", " << v2.z << ", " << std::endl;
-                std::cout << ScalarProd(v,v1) << ", " << ScalarProd(v,v2) << ", " << ScalarProd(v1,v1) << ", " << ScalarProd(v2,v2) << std::endl;
+                std::cout << ScalarProd(v, v1) << ", " << ScalarProd(v, v2) << ", " << ScalarProd(v1, v1) << ", " << ScalarProd(v2, v2) << std::endl;
                 return false;
             }
         }
@@ -114,10 +131,13 @@ bool PL_Complex::checkIfALLFacesAreFlat(){
     return true;
 }
 
-void PL_Complex::detectFeatures(double const sharpTheta, double const flatTheta){
+void PL_Complex::detectFeatures(double const sharpTheta, double const flatTheta)
+{
     /* Detect Sharp Edges */
-    for(auto& edge : edges){
-        if(edge->faces.size() == 1){
+    for (auto &edge : edges)
+    {
+        if (edge->faces.size() == 1)
+        {
             sharp_edges.push_back(edge);
             edge->isSharp = true;
             continue;
@@ -127,32 +147,36 @@ void PL_Complex::detectFeatures(double const sharpTheta, double const flatTheta)
 
         std::cout << "Edge " << edge->index << ", dihedral angle = " << dihedralAngle / M_PI << "*pi" << std::endl;
 
-        if(dihedralAngle < (M_PI - sharpTheta)){
+        if (dihedralAngle < (M_PI - sharpTheta))
+        {
             sharp_edges.push_back(edge);
             edge->isSharp = true;
             continue;
         }
 
-        if(dihedralAngle < (M_PI - flatTheta)){
+        if (dihedralAngle < (M_PI - flatTheta))
+        {
             std::cout << "ERROR: dihedral angle of edge" << edge->index << " is not sharp nor flat!!!!!" << std::endl;
             exit(1);
         }
 
         edge->isSharp = false;
     }
-    std::cout << "Sharp Edges:\n"; 
+    std::cout << "Sharp Edges:\n";
 
-    for(auto& edge : sharp_edges){
-        std::cout << "Edge: " << edge->index <<"\n"; 
+    for (auto &edge : sharp_edges)
+    {
+        std::cout << "Edge: " << edge->index << "\n";
     }
     std::cout << std::endl;
-    
+
     /* Detect Sharp Corners */
-    for(auto& vertex : vertices){
+    for (auto &vertex : vertices)
+    {
         std::vector<Edge> vertex_sharp_edges;
 
-        for(auto& edge : vertex->edges)
-            if(edge->isSharp)
+        for (auto &edge : vertex->edges)
+            if (edge->isSharp)
                 vertex_sharp_edges.push_back(edge);
 
         if(vertex_sharp_edges.size() > 2){
