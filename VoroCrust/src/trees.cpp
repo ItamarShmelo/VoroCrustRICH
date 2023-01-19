af86d0d484ca41a1a12594f0d3ac933094c71fa4
ItamarShmelo <itamargiron@gmail.com>
2023-01-19 18:09:17 +0200

    create ANNpointArray by uniformly super sampling the faces

--- VoroCrust/src/trees.cpp
+++ VoroCrust/src/trees.cpp
@@ -69,3 +69,65 @@ ANNpointArray Trees::superSampleEdges(std::vector<Edge> const& edges, std::size_
 
     return points;
 }
+
+ANNpointArray Trees::superSampleFaces(std::vector<Face> const& faces, std::size_t const Nsample){
+    boost::mt19937 rng(std::time(nullptr));
+    boost::random::uniform_01<> zeroone;
+    boost::variate_generator<boost::mt19937, boost::uniform_01<>> rand_gen(rng, zeroone);
+
+    std::vector<double> start_area(faces.size(), 0.0);
+
+    double total_area;
+    for(unsigned int i=0; i<faces.size(); ++i){
+        Face const& face = faces[i];
+        start_area[i] = total_area;
+
+        double const face_area = face->calcArea();
+        total_area += face_area;
+    }
+
+    std::cout << "\nFace Samples: \n---------------------\n";
+    ANNpointArray points = annAllocPts(Nsample, 3);
+
+    for(std::size_t i = 0; i<Nsample; ++i){
+        double const sample_area = rand_gen()*total_area;
+
+        auto const iter_lower_bound = std::lower_bound(start_area.begin(), start_area.end(), sample_area);
+        std::size_t face_index = std::distance(start_area.begin(), iter_lower_bound) - 1;
+
+        Face const& face = faces[face_index];
+        
+
+        //! TODO: add option for sampling for a face with more than 3 edges.
+        if(face->vertices.size() != 3){
+            std::cout << "ERROR: Algorith supports only triangular meshes for now" << std::endl;
+            exit(1);
+        }
+
+        // sample a point in a triangle
+        double const sqrt_r1 = std::sqrt(rand_gen());
+        double const r2 = rand_gen();
+
+        if(sqrt_r1 < 1e-14 || r2 < 1e-14){
+            //! TODO: make eps a user given parameter. 
+            i--;
+            continue;
+        }
+
+        Vector3D const& A = face->vertices[0]->vertex;
+        Vector3D const& B = face->vertices[1]->vertex;
+        Vector3D const& C = face->vertices[2]->vertex;
+
+        Vector3D point = (1.0-sqrt_r1)*A + (sqrt_r1*(1.0-r2))*B + (r2*sqrt_r1)*C;
+
+        points[i][0] = point.x;
+        points[i][1] = point.y;
+        points[i][2] = point.z;
+
+    }
+
+    return points;
+}
+
+
+
