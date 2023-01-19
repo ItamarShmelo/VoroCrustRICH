abd8cb64e348014dfd8f249de6454bb4cfad5be6
ItamarShmelo <itamargiron@gmail.com>
2023-01-19 18:08:22 +0200

    creates an ANNpointArray from a given set of vertices

--- VoroCrust/src/trees.cpp
+++ VoroCrust/src/trees.cpp
@@ -9,3 +9,18 @@ Trees::Trees(): kd_vertices(nullptr),
                 vertices_points(),
                 edges_points(),
                 faces_points() {}
+
+ANNpointArray Trees::pointsFromVertices(std::vector<Vertex> const& vertices){
+    std::size_t const Npoints = vertices.size();
+    ANNpointArray points = annAllocPts(Npoints, 3);
+
+    for(std::size_t i = 0; i<Npoints; ++i){
+        Vector3D vertex = vertices[i]->vertex;
+        
+        points[i][0] = vertex.x;
+        points[i][1] = vertex.y;
+        points[i][2] = vertex.z;
+    }
+    
+    return points;
+}
