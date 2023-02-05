a3cb02c91094a4866b5fe6dda8ec2b680fd50e5c
ItamarShmelo <itamargiron@gmail.com>
2023-02-05 15:18:57 +0200

    loads the eligble corners from a sharp_corners vector

--- VoroCrust/src/RMPS/CornersRMPS.cpp
+++ VoroCrust/src/RMPS/CornersRMPS.cpp
@@ -4,3 +4,8 @@
 CornersRMPS::CornersRMPS(double const maxRadius_, double const L_Lipschitz_) : maxRadius(maxRadius_), L_Lipschitz(L_Lipschitz_), eligble_corners() {}
 
 
+void CornersRMPS::loadCorners(std::vector<Vertex> const& sharp_corners){
+    eligble_corners.clear();
+    for(Vertex const& corner_ptr : sharp_corners)
+        eligble_corners.push_back(corner_ptr->vertex);
+}
