e6194d058a998cdcf77348d6fc76e486d4c1eade
ItamarShmelo <itamargiron@gmail.com>
2023-02-05 14:57:18 +0200

    constructor

---
+++ VoroCrust/src/RMPS/CornersRMPS.cpp
@@ -0,0 +1,6 @@
+#include "CornersRMPS.hpp"
+#include <boost/random.hpp>
+
+CornersRMPS::CornersRMPS(double const maxRadius_, double const L_Lipschitz_) : maxRadius(maxRadius_), L_Lipschitz(L_Lipschitz_), eligble_corners() {}
+
+
