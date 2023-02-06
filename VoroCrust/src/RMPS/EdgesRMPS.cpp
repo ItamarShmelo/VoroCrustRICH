243fd31b7f47d81b8fc5fb64a3fe3cab7cb563b4
ItamarShmelo <itamargiron@gmail.com>
2023-02-06 14:06:31 +0200

    divide eligble edges, each edge is divided at the midpoint to two new edges

--- VoroCrust/src/RMPS/EdgesRMPS.cpp
+++ VoroCrust/src/RMPS/EdgesRMPS.cpp
@@ -22,3 +22,22 @@ std::pair<double const, std::vector<double> const> EdgesRMPS::calculateTotalLeng
     return std::pair<double const, std::vector<double> const> (total_len, start_len);
 }
 
+void EdgesRMPS::divideEligbleEdges(){
+    std::vector<EligbleEdge> new_eligble_edges(2*eligble_edges.size(), EligbleEdge(Vector3D(0.0, 0.0, 0.0), Vector3D(0.0, 0.0, 0.0), 0));
+
+    std::size_t const num_of_eligble_edges = eligble_edges.size();
+    for(std::size_t i=0; i<num_of_eligble_edges; ++i){
+        EligbleEdge const& edge = eligble_edges[i];
+        Vector3D const& midpoint = 0.5*(edge[1] + edge[0]);
+
+        new_eligble_edges[2*i][0] = edge[0];
+        new_eligble_edges[2*i][1] = midpoint;
+        new_eligble_edges[2*i].crease_index = edge.crease_index;
+
+        new_eligble_edges[2*i+1][0] = midpoint;
+        new_eligble_edges[2*i+1][1] = edge[1];
+        new_eligble_edges[2*i+1].crease_index = edge.crease_index;
+    }
+
+    eligble_edges = new_eligble_edges;
+}
