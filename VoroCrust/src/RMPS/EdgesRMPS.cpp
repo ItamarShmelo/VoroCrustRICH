3b277bed3f8bea61738ada36297b4991b4f60d68
ItamarShmelo <itamargiron@gmail.com>
2023-02-06 14:05:26 +0200

    load the sharp edges to the eligble edges array

--- VoroCrust/src/RMPS/EdgesRMPS.cpp
+++ VoroCrust/src/RMPS/EdgesRMPS.cpp
@@ -1,3 +1,9 @@
 #include "EdgesRMPS.hpp"
 
 EdgesRMPS::EdgesRMPS(double const maxRadius_, double const L_Lipschitz_, double const alpha_) : maxRadius(maxRadius_), L_Lipschitz(L_Lipschitz_), alpha(alpha_), uni01_gen(boost::mt19937(std::time(nullptr)), boost::random::uniform_01<>()) {}
+
+void EdgesRMPS::loadEdges(std::vector<Edge> const& sharp_edges){
+    for(Edge const& edge : sharp_edges)
+        eligble_edges.push_back(EligbleEdge(edge->vertex1->vertex, edge->vertex2->vertex, edge->crease_index));
+}
+
