f198a33919f6ce2fffecc44d616994f74a68adc8
ItamarShmelo <itamargiron@gmail.com>
2023-02-09 14:19:18 +0200

    decleration of private methods used used by edgesRMPS

--- VoroCrust/src/RMPS/EdgesRMPS.hpp
+++ VoroCrust/src/RMPS/EdgesRMPS.hpp
@@ -57,23 +57,30 @@ class EdgesRMPS {
         void divideEligbleEdges();
 
         //! \brief checks if `p` is deeply covered by any ball in edges_ball_tree
-        bool checkIfPointIsDeeplyCovered(Vector3D const& p, VoroCrust_KD_Tree_Ball const& edges_ball_tree) const;
+        bool checkIfPointIsDeeplyCovered(Vector3D const& p, VoroCrust_KD_Tree_Ball const& edges_ball_tree, VoroCrust_KD_Tree_Ball const& corners_ball_tree) const;
 
         /*! \brief sample the eligble edges
             \return <success, sample> where success is a bool flag and is false if sampling failed
         */
-        std::pair<bool, Vector3D> sampleEligbleEdges(double const total_len, std::vector<double> const& start_len);
+        std::tuple<bool, std::size_t const, Vector3D const> sampleEligbleEdges(double const total_len, std::vector<double> const& start_len);
 
         //! \brief discard any eligble edges that meet the critrea for discardtion 
-        void discardEligbleEdges(Trees const& trees);
+        void discardEligbleEdges(VoroCrust_KD_Tree_Ball &edges_ball_tree, Trees const& trees);
 
         //! \brief discard any eligble edge fully contained inside a corner ball
         void discardEligbleEdgesContainedInCornerBalls(VoroCrust_KD_Tree_Ball const& corners_ball_tree);
 
-        //! \brief checks if `edge` is co smooth with ball at ball_index;
-        bool isEdgeBallCoSmoothWithEligbleEdge(EligbleEdge const& edge, VoroCrust_KD_Tree_Ball const& edges_ball_tree, std::size_t const ball_index);
+        //! \brief enfoce that the ball is co smooth with with every point on the crease that is in its radius.
+        void enforceEdgeBallCoSmoothWithEligbleEdge(VoroCrust_KD_Tree_Boundary const& edges_boundary_tree, VoroCrust_KD_Tree_Ball &edges_ball_tree, std::size_t const ball_index);
 
+        //
+        double calculateSmoothnessLimitation(Vector3D const& center, Vector3D const& parallel, std::size_t const feature_index, VoroCrust_KD_Tree_Boundary const& edges_boundary_tree) const;
 
+        bool isEligbleEdgeIsDeeplyCoveredInEdgeBall(EligbleEdge const& edge, VoroCrust_KD_Tree_Ball const& edges_ball_tree, std::size_t const ball_index);
+
+        double calculateInitialRadius(Vector3D const& point, std::size_t const edge_index, VoroCrust_KD_Tree_Ball const& edges_ball_tree, VoroCrust_KD_Tree_Boundary const& edges_boundary_tree) const;
+
+        bool isBallContainesExistingSample(Vector3D const& center, double const radius, VoroCrust_KD_Tree_Ball const& edges_ball_tree) const;
 };
 
 #endif // EDGES_RMPS
