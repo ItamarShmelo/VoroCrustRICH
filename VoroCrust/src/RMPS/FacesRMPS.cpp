d7532b205867aea8adef36e34cd0b661da6a4dac
ItamarShmelo <itamargiron@gmail.com>
2023-02-14 15:21:03 +0200

    implementation of loadFaces

--- VoroCrust/src/RMPS/FacesRMPS.cpp
+++ VoroCrust/src/RMPS/FacesRMPS.cpp
@@ -12,3 +12,19 @@ FacesRMPS::FacesRMPS(double const maxRadius_,
                                     uni01_gen(boost::mt19937(std::time(nullptr)), boost::random::uniform_01<>()), 
                                     plc(plc_), 
                                     eligble_faces() {}
+
+void FacesRMPS::loadFaces(std::vector<Face> const& faces) {
+    if(not eligble_faces.empty()){
+        std::cout << "eligble faces is not empty when loading faces" << std::endl;
+        exit(1);
+    }
+
+    for(Face const& face : faces){
+        std::vector<Vector3D> vertices;
+        for(Vertex const& vertex : face->vertices) {
+            vertices.push_back(vertex->vertex);
+        }
+
+        eligble_faces.push_back(EligbleFace(vertices, face->patch_index, face->index));
+    }
+}                                 
