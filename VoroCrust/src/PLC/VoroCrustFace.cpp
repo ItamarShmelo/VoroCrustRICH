b50962fb3ea5435fc4644dc307e9174f14323e7c
ItamarShmelo <itamargiron@gmail.com>
2023-02-27 18:25:45 +0200

    implementation of pointIsInsideFace

--- VoroCrust/src/PLC/VoroCrustFace.cpp
+++ VoroCrust/src/PLC/VoroCrustFace.cpp
@@ -93,26 +93,27 @@ std::pair<bool, Vector3D> VoroCrustFace::pointXYaxisRayIntersectsAt(Vector3D con
 
     return std::pair<bool, Vector3D>(true, Vector3D(point.x, point.y, z_intersect_plane));
 }
-std::pair<bool, Vector3D> VoroCrustFace::pointXYaxisRayIntersectsAt(Vector3D const& point) const {
-    Vector3D const& p1 = vertices[0]->vertex;
-    Vector3D const& p2 = vertices[1]->vertex;
-    Vector3D const& p3 = vertices[2]->vertex;
 
-    double const coeff_z = Mat33<double>(p1.x, p1.y, 1.0, p2.x, p2.y, 1.0, p3.x, p3.y, 1.0).determinant();
-    // face is parallel to ray return sucess = false
-    if(std::abs(coeff_z) < 1e-14) {
-        return std::pair<bool, Vector3D>(false, Vector3D(0.0, 0.0, 0.0));
+bool sameSide(Vector3D const& p1, Vector3D const& p2, Vector3D const& a, Vector3D const& b){
+    // https://blackpawn.com/texts/pointinpoly/
+
+    return ScalarProd(CrossProduct(b-a, p1-a), CrossProduct(b-a, p2-a)) >= 0;
+}
+
+bool VoroCrustFace::pointIsInsideFace(Vector3D const& point) const {
+    //! SHOULDJUSTCHECKITONCE: should just sheck it once
+    if(vertices.size() != 3){
+        std::cout << "ERROR: face is not a triangle!" << std::endl;
+        exit(1);
     }
 
-    double const coeff_x = Mat33<double>(p1.y, p1.z, 1.0, p2.y, p2.z, 1.0, p3.y, p3.z, 1.0).determinant();
-    double const coeff_y = -Mat33<double>(p1.x, p1.z, 1.0, p2.x, p2.z, 1.0, p3.x, p3.z, 1.0).determinant();
-    
-    double const value = -Mat33<double>(p1.x, p1.y, p1.z, p2.x, p2.y, p2.z, p3.x, p3.y, p3.z).determinant();
+    Vector3D const& a = vertices[0]->vertex;
+    Vector3D const& b = vertices[1]->vertex;
+    Vector3D const& c = vertices[2]->vertex;
 
-    double const z_intersect_plane = -(coeff_x*point.x + coeff_y*point.y + value)/coeff_z;
-
-    return std::pair<bool, Vector3D>(true, Vector3D(point.x, point.y, z_intersect_plane));
+    return sameSide(point, a, b, c) && sameSide(point, b, a, c) && sameSide(point, c, a, b);
 }
+
 std::string VoroCrustFace::repr() const
 {
     std::ostringstream s;
