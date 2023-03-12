1e38b081283efa0e33d5df44807b539e200af37d
ItamarShmelo <itamargiron@gmail.com>
2023-03-12 15:45:18 +0200

    this weird dependecy remains because weak_ptr is not a real ptr and its annoying

--- VoroCrust/src/PLC/VoroCrustUsing.hpp
+++ VoroCrust/src/PLC/VoroCrustUsing.hpp
@@ -11,8 +11,4 @@ using Vertex = std::shared_ptr<VoroCrustVertex>;
 using Edge = std::shared_ptr<VoroCrustEdge>;
 using Face = std::shared_ptr<VoroCrustFace>;
 
-using VertexWeakPtr = std::weak_ptr<VoroCrustVertex>;
-using EdgeWeakPtr = std::weak_ptr<VoroCrustVertex>;
-using FaceWeakPtr = std::weak_ptr<VoroCrustVertex>;
-
 #endif /* VOROCRUST_USING */
