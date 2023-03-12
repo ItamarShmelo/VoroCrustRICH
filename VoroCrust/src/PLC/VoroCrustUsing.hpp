0db244447a7cfa58a5e9359581c49a2dc87356ed
ItamarShmelo <itamargiron@gmail.com>
2023-03-12 15:32:23 +0200

    decleration of weak ptrs

--- VoroCrust/src/PLC/VoroCrustUsing.hpp
+++ VoroCrust/src/PLC/VoroCrustUsing.hpp
@@ -11,4 +11,8 @@ using Vertex = std::shared_ptr<VoroCrustVertex>;
 using Edge = std::shared_ptr<VoroCrustEdge>;
 using Face = std::shared_ptr<VoroCrustFace>;
 
+using VertexWeakPtr = std::weak_ptr<VoroCrustVertex>;
+using EdgeWeakPtr = std::weak_ptr<VoroCrustVertex>;
+using FaceWeakPtr = std::weak_ptr<VoroCrustVertex>;
+
 #endif /* VOROCRUST_USING */
