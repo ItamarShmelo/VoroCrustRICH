9d3202351dee6f4e66545c14b8ff74904674fc82
ItamarShmelo <itamargiron@gmail.com>
2024-12-18 13:40:29 +0200

    removed binding of VoroCrustAlgorithm

--- src/_VoroCrustAlgorithm.cpp
+++ src/_VoroCrustAlgorithm.cpp
@@ -6,31 +6,6 @@
 void bind_vorocrust_algorithm(pybind11::module& m) {
     using namespace pybind11::literals;
     
-    pybind11::class_<VoroCrustAlgorithm, VoroCrustAlgorithmPtr>(m, "VoroCrustAlgorithm")
-        .def(pybind11::init<PL_Complex const&, 
-                            double const,
-                            double const, 
-                            double const, 
-                            double const, 
-                            std::size_t const,
-                            std::size_t const,
-                            std::size_t const>(),
-                            pybind11::kw_only(),
-                            "plc"_a,
-                            "sharpTheta"_a,
-                            "maxRadius"_a,
-                            "L_Lipschitz"_a,
-                            "alpha"_a,
-                            "maximal_num_iter"_a,
-                            "num_of_samples_edges"_a,
-                            "num_of_samples_faces"_a)
-        .def("run", &VoroCrustAlgorithm::run)
-        .def("dump", &VoroCrustAlgorithm::dump, pybind11::kw_only(), "dirname"_a)
-        .def("load_dump", &VoroCrustAlgorithm::load_dump, pybind11::kw_only(), "dirname"_a)
-        .def("getSeeds", &VoroCrustAlgorithm::getSeeds)
-        .def_readonly("plc", &VoroCrustAlgorithm::plc)
-        .def_readonly("trees", &VoroCrustAlgorithm::trees);
-    
     m.def("dumpSeeds", &dumpSeeds, pybind11::kw_only(), "dirname"_a, "seeds"_a);
    
     m.def("determineZoneOfSeeds", &determineZoneOfSeeds, pybind11::kw_only(), "seeds"_a, "zone_plcs"_a);
